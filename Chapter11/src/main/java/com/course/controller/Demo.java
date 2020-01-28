f6a543f7c51aee84a834e8a2d374505b905cd052
liu.yang <liu.yang@mljr.com>
2020-01-28 14:08:37 +0800

    完成insert数据

--- Chapter11/src/main/java/com/course/controller/Demo.java
+++ Chapter11/src/main/java/com/course/controller/Demo.java
@@ -1,13 +1,12 @@
 package com.course.controller;
 
+import com.course.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
@@ -26,6 +25,13 @@ public class Demo {
 
         return template.selectOne("getUserCount");
 
+    }
+
+    @RequestMapping(value = "addUser", method = RequestMethod.POST)
+    @ApiOperation(value = "这是一组新增用户数据的接口", httpMethod = "POST")
+    public int addUser(@RequestBody User user){
+
+        return template.insert("addUser", user);
 
     }
 }
